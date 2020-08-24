3eb6ed84e0f5d851c96aa13c9ff90d38f3bab3e4
shohey-421 <shohey-421@users.noreply.github.com>
2020-08-24 17:53:52 +0900

    Userモデルの単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ApplicationRecord
     validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
     validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
     validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
+    validates :birthday
   end
 
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i ,message: "Include both letters and numbers"}
