eed5d4c183026c426d96c8f2ccab1df6f285c9c5
shohey-421 <shohey-421@users.noreply.github.com>
2020-08-26 16:44:59 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,12 +6,16 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :shipping_date
 
-  with_options numericality: { other_than: 1 } do
-    validates :category_id
-    validates :condition_id	
-    validates :shipping_burden_id
-    validates :ship_from_id
-    validates :shipping_date_id
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :category_id, numericality: { other_than: 1 }
+    validates :condition_id	, numericality: { other_than: 1 }
+    validates :shipping_burden_id, numericality: { other_than: 1 }
+    validates :ship_from_id, numericality: { other_than: 1 }
+    validates :shipping_date_id, numericality: { other_than: 1 }
+    validates :price, between: {300,9999999}, format: { with: /^[0-9]+$/, message: "Full-width characters"}
   end
 
   belongs_to :user
