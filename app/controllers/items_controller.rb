d1ef8e0a82c1ecad15270241dfa60dc9ae8a79c4
shohey-421 <shohey-421@users.noreply.github.com>
2020-08-26 17:31:23 +0900

    エラーメッセージの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,22 @@ class ItemsController < ApplicationController
   end
 
   def new
-    
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      return redirect_to root_path
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_burden_id, :ship_from_id, :shipping_date_id, :price)
   end
 end
