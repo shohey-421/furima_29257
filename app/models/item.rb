e7b66a2b85f446ea396d582c63045939ce4f8a5e
shohey-421 <shohey-421@users.noreply.github.com>
2020-08-26 18:09:11 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,18 +6,27 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :shipping_date
 
+  belongs_to :user
+
+  has_one_attached :image
+  validate :image_presence
+  def image_presence
+    unless image.attached? then
+      errors.add(:image, "can't be blank")
+    end
+  end
+  
   with_options presence: true do
-    validates :image
     validates :name
     validates :description
-    validates :category_id, numericality: { other_than: 1 }
-    validates :condition_id	, numericality: { other_than: 1 }
-    validates :shipping_burden_id, numericality: { other_than: 1 }
-    validates :ship_from_id, numericality: { other_than: 1 }
-    validates :shipping_date_id, numericality: { other_than: 1 }
-    validates :price, between: {300,9999999}, format: { with: /^[0-9]+$/, message: "Full-width characters"}
+    validates :category_id, numericality: { other_than: 1, message: "Select"}
+    validates :condition_id	, numericality: { other_than: 1, message: "Select"}
+    validates :shipping_burden_id, numericality: { other_than: 1, message: "Select"}
+    validates :ship_from_id, numericality: { other_than: 1, message: "Select" }
+    validates :shipping_date_id, numericality: { other_than: 1, message: "Select"}
+    validates :price, format: { with: /\A[0-9]+\z/, message: "Half-width number"}
   end
 
-  belongs_to :user
-  has_one_attached :image
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range"}
+
 end
