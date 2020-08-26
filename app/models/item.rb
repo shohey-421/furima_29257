f3306e01b1445f2f0a563c1993987ae000e60acc
shohey-421 <shohey-421@users.noreply.github.com>
2020-08-26 14:59:55 +0900

    ActiveHash各モデルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,18 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_burden
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping_date
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :condition_id	
+    validates :shipping_burden_id
+    validates :ship_from_id
+    validates :shipping_date_id
+  end
+
   belongs_to :user
 end
