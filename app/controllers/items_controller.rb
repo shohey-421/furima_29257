1fae5f36b84cf77d38493109a22ab4eae49d4012
shohey-421 <shohey-421@users.noreply.github.com>
2020-08-27 15:57:03 +0900

    アクションの整理とログインユーザー確認の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user! ,only: [:new, :create, :edit, :update]
+  before_action :authenticate_user! ,only: [:new, :create]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     
@@ -15,20 +17,17 @@ class ItemsController < ApplicationController
       @item.save
       return redirect_to root_path
     else
-      render "new"
+      render :new
     end
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     @item.update(item_params)
     if @item.valid?
       return redirect_to item_path(@item)
@@ -42,4 +41,15 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_burden_id, :ship_from_id, :shipping_date_id, :price).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in? && @item.user_id == current_user.id
+      redirect_to root_path
+    end
+  end
+
 end
