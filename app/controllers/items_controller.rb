381b6ce047b156bdb9d6d40a2ee4fdd879f7fd73
shohey-421 <shohey-421@users.noreply.github.com>
2020-08-27 15:46:27 +0900

    編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user! ,only: [:new, :create]
+  before_action :authenticate_user! ,only: [:new, :create, :edit, :update]
 
   def index
     
@@ -28,8 +28,13 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.valid?
+      return redirect_to item_path(@item)
+    else
+      render :edit
+    end
   end
 
   private
