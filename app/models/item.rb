ea8a959d4367414d4052e2ecb019d01d37a5e148
shohey-421 <shohey-421@users.noreply.github.com>
2020-08-27 11:42:55 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -24,7 +24,7 @@ class Item < ApplicationRecord
     validates :shipping_burden_id, numericality: { other_than: 0, message: "Select"}
     validates :ship_from_id, numericality: { other_than: 0, message: "Select" }
     validates :shipping_date_id, numericality: { other_than: 0, message: "Select"}
-    validates :price, format: { with: /\A[0-9]+\z/, message: "Half-width number"}
+    validates :price, numericality: { only_integer: true, message: "Half-width number"}
   end
 
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range"}
