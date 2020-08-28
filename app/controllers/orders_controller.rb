3b67e568742fd52e431b9ecdd0fc80dd77e22506
shohey-421 <shohey-421@users.noreply.github.com>
2020-08-28 09:59:38 +0900

    クレジット決済部分を除く商品購入機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,24 @@
 class OrdersController < ApplicationController
 
   def index
+    @order = OrderAddress.new
     @item = Item.find (params[:item_id])
   end
 
   def create
+    @order = OrderAddress.new(order_params)
+    if @order.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
-  
+
+  private
+
+  def order_params
+    params_item_id = params.permit(:item_id)
+    params.require(:order_address).permit(:postcode, :region_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id,item_id: params_item_id[:item_id])
+  end
+
 end
---
+++ app/forms/order_address.rb
@@ -0,0 +1,19 @@
+class OrderAddress
+
+  include ActiveModel::Model
+
+  attr_accessor :postcode, :region_id, :city, :postal_code, :prefecture, :city, :block, :building, :phone_number, :user_id, :item_id
+
+  with_options presence: true do
+    validates :postcode,      format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Include hyphen(-)"}
+    validates :region_id,     numericality: { other_than: 0, message: "Select"}
+    validates :city
+    validates :block
+    validates :phone_number
+  end
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postcode: postcode, region_id: region_id, city: city, block: block, building: building, phone_number:phone_number, order_id: order.id)
+  end
+end
