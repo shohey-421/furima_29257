2810fa147558e4d7a4d18315f366db71101b793a
shohey-421 <shohey-421@users.noreply.github.com>
2020-09-01 17:37:04 +0900

    コメント取得の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,7 @@ class ItemsController < ApplicationController
 
   def show
     @comment = Comment.new
-    @comments = Comment.all
+    @comments = Comment.where(item_id: params[:id])
   end
 
   def edit
