22884dd9ebefee16037d29acf62ba3852215b4b4
shohey-421 <shohey-421@users.noreply.github.com>
2020-09-03 17:19:26 +0900

    検索機能微調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,7 @@ class ApplicationController < ActionController::Base
     @categories = Category.where.not(id:0)
     @condition = Condition.where.not(id:0)
     @categories = Category.where.not(id:0)
+    @shipping_burden = ShippingBurden.where.not(id:0)
   end
 
   protected
